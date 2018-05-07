e6310a2be1b900c67577d429f3cf7f263c3de6dd
Kayla Ochoa <ochoa.kayla@yahoo.com>
2018-05-07 20:02:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+def line(katz_deli)
+  if katz_deli == []
+  puts  "The line is currently empty."
+else
+  current_line= "The line is currently:"
+  katz_deli.each_with_index do |person, index|
+    current_line << " #{index+1}. #{person}"
+end
+puts current_line
+end
+
+def take_a_number(katz_deli, name)
+  if katz_deli.empty?
+  katz_deli<< "#{name}"
+else
+  katz_deli<<katz_deli.each_with_index do |name, index|
+ puts "Welcome, #{name}. You are number #{index+1} in line."
+  end
+end
+end
